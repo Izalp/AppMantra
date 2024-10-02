4455e0be8a2f4310de5d594388783fdef7bea542
Iza Lopes <102091381+Izalp@users.noreply.github.com>
2024-10-02 19:57:43 -0300

    :wrench: chore: configurar Firebase

---
+++ meditacao-app/src/firebase/config.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAcEXn0hwIpc8yu31I8I5R5oWjr5Bap8PU",
+  authDomain: "dbmantra-8ac00.firebaseapp.com",
+  projectId: "dbmantra-8ac00",
+  storageBucket: "dbmantra-8ac00.appspot.com",
+  messagingSenderId: "396911581837",
+  appId: "1:396911581837:web:0cb1c0d9041f433c0620c4",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
